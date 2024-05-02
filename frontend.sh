bf95a28f724dd5e655b6a4ab75c06d261113167f
viswaroop <vissu.roop.87@gmail.com>
2024-05-02 21:52:39 +0530

    roboshop commends

--- frontend.sh
+++ frontend.sh
@@ -8,12 +8,12 @@ rm -rf /usr/share/nginx/html/* &>>${log_file}
 print_head "downloading fontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-print_head "exctacting downloading frontend"
+print_head "extracting downloading frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
 print_head "coping nginx roboshop for config"
-cp configs/nginx-roboshop-conf etc/nginx/default.d/roboshop.conf &>>${log_file}
+cp ${cod_log}/config/nginx-roboshop-conf etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
