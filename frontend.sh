5770cdda0d7ccc5bbd3bcd8087e33753f3087f71
viswaroop <vissu.roop.87@gmail.com>
2024-05-02 17:20:19 +0530

    roboshop commends

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file}
 print_head(){
-  echo -e "/e[35m$1/e[0m"
+  echo -e " /e[35m$1/e[0m "
 }
 print_head "installing nginx"
 dnf install nginx -y &>>${log_file}
@@ -20,8 +20,8 @@ unzip /tmp/frontend.zip &>>${log_file}
 print_head "coping nginx roboshop for config"
 cp configs/nginx-roboshop-conf etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-print_head"enabling nginx"
+print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
 
-print_head" starting nginx"
+print_head "starting nginx"
 systemctl restart nginx &>>${log_file}
