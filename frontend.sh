5d90f71a2c32a744f6db3cd9046913034723161d
viswaroop <vissu.roop.87@gmail.com>
2024-05-02 15:52:13 +0530

    roboshop commends

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,15 @@
+echo -e"[35m installing nginx/[0"
 dnf install nginx -y
+echo -e"[35m removing old content/[0"
 rm -rf /usr/share/nginx/html/*
+echo -e"[35m downloading fontend content/[0"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e"[35m exctacting downloading frontend/[0"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo -e"[35m coping nginx roboshop for config/[0"
 cp configs/nginx-roboshop-conf etc/nginx/default.d/roboshop.conf
+echo -e"[35m enabling nginx/[0"
 systemctl enable nginx
+echo -e"[35m starting nginx/[0"
 systemctl restart nginx
