75aa22eb4b4723706b7e942a1e42f3ed2f1dcb17
viswaroop <vissu.roop.87@gmail.com>
2024-05-02 17:38:03 +0530

    roboshop commends

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,14 @@
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+print_head(){
+  echo -e " \e[35m$1\e[0m "
+}
+print_head"setup mongodb repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+print_head"install mongodb"
 dnf install mongodb-org -y
+print_head"enable mongodb"
 systemctl enable mongod
+print_head"start mongodb services"
 systemctl start mongod
