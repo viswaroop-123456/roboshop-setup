176610a9767a975a2b8091b00f46b3a5b5fafeb2
viswaroop <vissu.roop.87@gmail.com>
2024-05-02 17:15:47 +0530

    roboshop commends

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,27 @@
 code_dir=$(pwd)
 log_file=/tmp/roboshop.log
-rm-f ${log_file}
+rm -f ${log_file}
 print_head(){
   echo -e"/e[35m$1/e[0m"
 }
 print_head "installing nginx"
-dnf install nginx -y&>>${log_file}
+dnf install nginx -y &>>${log_file}
+
 print_head "removing old content"
-rm -rf /usr/share/nginx/html/*&>>${log_file}
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+
 print_head "downloading fontend content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip&>>${log_file}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+
 print_head "exctacting downloading frontend"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip&>>${log_file}
+unzip /tmp/frontend.zip &>>${log_file}
+
 print_head "coping nginx roboshop for config"
-cp configs/nginx-roboshop-conf etc/nginx/default.d/roboshop.conf&>>${log_file}
+cp configs/nginx-roboshop-conf etc/nginx/default.d/roboshop.conf &>>${log_file}
+
 print_head"enabling nginx"
-systemctl enable nginx&>>${log_file}
+systemctl enable nginx &>>${log_file}
+
 print_head" starting nginx"
-systemctl restart nginx&>>${log_file}
+systemctl restart nginx &>>${log_file}
