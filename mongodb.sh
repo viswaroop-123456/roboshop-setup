1a21ab0241c8b113187a61c7123056e8a51725fb
viswaroop <vissu.roop.87@gmail.com>
2024-05-02 20:14:29 +0530

    roboshop commends

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ print_head "install mongodb"
 dnf install mongodb-org -y &>>${log_file}
 
 print_head "update mongodb listen address"
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf &>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 
 print_head "enable mongodb"
 systemctl enable mongod &>>${log_file}
