1f5963468fa009787a7f4fe54a3e588ffbf08fd4
viswaroop <vissu.roop.87@gmail.com>
2024-05-03 13:50:57 +0530

    roboshop commends

---
+++ user.sh
@@ -0,0 +1,72 @@
+source common.sh
+
+print_head "configure nodejs repo"
+#dnf module disable nodejs -y
+#dnf module enable nodejs:18 -y &>>${log_file}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "install nodejs "
+dnf install nodejs -y &>>${log_file}
+status_check $?
+
+print_head "create roboshop user"
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+useradd roboshop &>>${log_file}
+fi
+status_check $?
+
+print_head "make directory"
+if [ ! -d /app ]; then
+mkdir /app &>>${log_file}
+fi
+status_check $?
+
+print_head "delete old content"
+rm -rf /app/* &>>${log_file}
+status_check $?
+
+print_head "downloading app content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+cd /app
+status_check $?
+
+print_head "extracting app content"
+unzip /tmp/catalogue.zip &>>${log_file}
+status_check $?
+
+print_head "installing nodejs dependencies"
+npm install &>>${log_file}
+status_check $?
+
+print_head "copy systemd service file"
+cp ${cod_dir}/config/user.service /etc/systemd/system/user.service &>>${log_file}
+status_check $?
+
+print_head "reload systemd"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+print_head "enable user services"
+systemctl enable user &>>${log_file}
+status_check $?
+
+print_head "start user services"
+systemctl restart user &>>${log_file}
+status_check $?
+
+print_head "copy mongo repo file "
+cp ${cod_dir}/config/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+status_check $?
+
+print_head "install mongo client"
+dnf install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_head "load schema"
+mongo --host mongodb.devopsbatch.cloud </app/schema/catalogue.js &>>${log_file}
+status_check $?
+
+
+
