595471071d5f89a0730665b3f89925b294c94ebc
viswaroop <vissu.roop.87@gmail.com>
2024-05-03 11:38:45 +0530

    roboshop commends

--- common.sh
+++ common.sh
@@ -6,3 +6,12 @@ rm -f ${log_file}
 print_head(){
   echo -e " \e[35m$1\e[0m "
 }
+
+status_check(){
+  if [ $1 -eq o ]; then
+    echo SUCCESS
+    else
+    echo FAILURE
+    fi
+
+}
--- frontend.sh
+++ frontend.sh
@@ -2,22 +2,29 @@ source common.sh
 
 print_head "installing nginx"
 dnf install nginx -y &>>${log_file}
+status_check $?
 
 print_head "removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
 
 print_head "downloading fontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
 
 print_head "extracting downloading frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
 
 print_head "coping nginx roboshop for config"
 cp ${cod_dir}/config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
 
 print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
+status_check $?
 
 print_head "starting nginx"
 systemctl start nginx &>>${log_file}
+status_check $?
